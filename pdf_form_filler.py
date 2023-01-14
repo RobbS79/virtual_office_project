c7c1fbe85fc0561e5740e48bb5ca5dfbdce2e6a2
Robert Soroka <74800762+RobbS79@users.noreply.github.com>
2023-01-14 21:36:20 +0100

    Update and rename pdf_form_filler.ipynb to pdf_form_filler.py

---
+++ pdf_form_filler.py
@@ -0,0 +1,63 @@
+import pandas as pd
+from PyPDF2 import PdfReader, PdfWriter
+import pandas.io.sql as psql
+import psycopg2
+
+###Eighth working attempt - for all pages filling text fields from Text8[0] to Text22[0] with values from employees database
+###Eighth working attempt - for all pages filling text fields from Text8[0] to Text22[0] with values from employees database
+###Eighth working attempt - for all pages filling text fields from Text8[0] to Text22[0] with values from employees database
+###Eighth working attempt - for all pages filling text fields from Text8[0] to Text22[0] with values from employees database
+###Eighth working attempt - for all pages filling text fields from Text8[0] to Text22[0] with values from employees database
+
+###         MMMMMMM     MMMMMMM        VVVV      VVVV       PPPPPPPPPPPP
+###         MMMMMMMM   MMMMMMMM         VVVV    VVVV        PPPP    PPPPP
+###         MMMM MMMMMMMMM MMMM          VVVV  VVVV         PPPP    PPPPP
+###         MMMM  MMMMMM   MMMM           VVV  VVV          PPPPPPPPPPPPP
+###         MMMM   MMMM    MMMM            VVVVVV           PPPP
+###         MMMM           MMMM             VVVV            PPPP
+###         MMMM           MMMM              VV             PPPP
+
+
+# Conneect to an existing databasee
+conn = psycopg2.connect("dbname=zentak_employees user=rob")
+
+
+emp_database_df = psql.read_sql('SELECT * FROM employees', conn)
+df = emp_database_df
+
+company_data = pd.read_excel("/Users/rob/Desktop/virtual_office/HR/forms/input_employees_to_db.xlsx","Sheet2",header=0)
+
+
+writer = PdfWriter()
+reader2 = PdfReader("forms/Oznameni_vyslani_pracovnika (1).pdf")
+fields2 = reader2.get_fields()
+#x2 = fields2.items()
+
+count = 0
+for j in range(len(reader2.pages)):
+    page = reader2.pages[j]
+    fields2 = reader2.get_fields()
+    #print(fields2)
+
+    writer.add_page(page)
+
+
+    for i in fields2.keys():
+        #print(i)
+
+        v = fields2[str(i)]["/T"]
+
+        if "Text" in v and count in range(8):
+            writer.update_page_form_field_values(
+                writer.pages[j], {v:f"{company_data.iloc[count][1]}"}
+            )
+            #print(f"{company_data.iloc[count][1]}")
+
+    
+        count += 1
+
+
+
+# write "output" to PyPDF2-output.pdf
+with open("filled-out2_3.pdf", "wb") as output_stream:
+    writer.write(output_stream)
